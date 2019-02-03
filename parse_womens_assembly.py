70a34e91a8b026b2ab43be11206ab3078ea55ae5
Farai Gandiya <dev@fgandiya.me>
2019-02-03 15:27:14 -0500

    Fixed parsing the womends assembly

--- parse_womens_assembly.py
+++ parse_womens_assembly.py
@@ -1,12 +1,12 @@
 import csv
 import os
-
+from party_formats import sanitize_party
 source_file_loc = os.path.join(os.getcwd(), "womens-assembly.txt")
 parsed_file_loc = os.path.join(os.getcwd(), "womens-assembly.csv")
 
 with open(parsed_file_loc, "w", newline="\n") as dest_file:
     csvwriter = csv.writer(dest_file)
-    headers = ["Province", "SER", "NAME", "GENDER", "PARTY"]
+    headers = ["Province", "SER", "Name", "Gender", "Party"]
     csvwriter.writerow(headers)
     with open(source_file_loc, "r") as source_file:
         lines = source_file.readlines()
@@ -17,4 +17,5 @@ with open(parsed_file_loc, "w", newline="\n") as dest_file:
                 name = lines[candidate_line_no+1].rstrip("\n")
                 gender = lines[candidate_line_no+2].rstrip("\n")
                 party = lines[candidate_line_no+3].rstrip("\n")
+                party = sanitize_party(party)
                 csvwriter.writerow((province, ser, name, gender, party))

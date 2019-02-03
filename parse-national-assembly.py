9b714ff0bfbbc26be29689e25a5ae1f52c67ea7b
Farai Gandiya <dev@fgandiya.me>
2019-02-03 15:21:32 -0500

    fixed the national asembly parsing

--- parse-national-assembly.py
+++ parse-national-assembly.py
@@ -1,14 +1,13 @@
 import csv
 import os
 import string
-
+from party_formats import sanitize_party
 provinces_dir = os.path.join(os.getcwd(), "national-assembly")
 
 with open("national_assembly.csv", "w", newline="\n") as national_assembly:
     headers = ["Province", "Constituency", "Name", "Gender", "Party", "Votes"]
     csvwriter = csv.writer(national_assembly)
     csvwriter.writerow(headers)
-    tot = 0
     for province_dir in os.listdir(provinces_dir):
         province = province_dir.replace("-", " ").title()
         province_dir_loc = os.path.join(provinces_dir, province_dir)
@@ -18,15 +17,12 @@ with open("national_assembly.csv", "w", newline="\n") as national_assembly:
             with open(os.path.join(province_dir_loc, const_file)) as prov_file:
                 lines = prov_file.readlines()
                 lines = list(map(lambda x: x.replace("\n", ""), lines))
-                tot +=1
                 for i in range(0, len(lines), 4):
-                    name = lines[i].rstrip(" ").title()
+                    name = lines[i].rstrip(" ").title().replace('  ', " ")
                     gender = lines[i+1].strip(" ")
                     party = lines[i+2].rstrip(" ")
+                    party = sanitize_party(party)
                     votes = int(lines[i+3].replace(" ", ""))
                     row = [province, const, name, gender, party, votes]
-                    csvwriter.writerow(row)
-
-print(tot)
-                    
+                    csvwriter.writerow(row)        
 

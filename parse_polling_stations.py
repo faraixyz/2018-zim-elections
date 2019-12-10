3ec6055c104847ae59de219ee291a7b0a12571bf
Farai Gandiya <gandiyafarai@gmail.com>
2019-12-10 16:13:11 +0200

    Removed white space

--- parse_polling_stations.py
+++ parse_polling_stations.py
@@ -7,7 +7,6 @@ def remove_extra_tags(soup):
     Removes the headers, footer and endOfContent marker in each page
     :param soup: A beautiful soup object
     """
-
     footer_text = re.compile(r"\d+ of 178")
     page_footers = soup.find_all(text=footer_text)
     page_headers = soup.find_all(text="2018 VOTER POPULATION PER POLLING STATION")
